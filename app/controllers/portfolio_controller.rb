1d5d2de6df70de0a1e70e54dfcd08937e880316a
will love <wlove29@gmail.com>
2015-04-21 16:53:24 -0400

    added portfolio page

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,5 @@
+class PortfolioController < ApplicationController
+	def portfolio
+		render(:portfolio)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/home' => 'home#website'
+  get '/portfolio' => 'portfolio#portfolio'
 end
